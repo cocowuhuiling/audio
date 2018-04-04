48805138e3deba1ba2432daace881fe737d00946
coco <1660656859@qq.com>
2018-04-04 18:02:28 +0800

    首页

---
+++ js/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    arrows: true,
+    nextArrow: ".next-slick",
+    prevArrow: ".prev-slick",
+    dots: true,
+    speed: 1500,
+    accessibility: true,
+  });
+});
