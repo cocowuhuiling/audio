d8fec9742a64fdd732db54b3450f9a893dc72d1e
coco <1660656859@qq.com>
2018-04-19 17:28:59 +0800

    fix

--- js/liminaudio.js
+++ js/liminaudio.js
@@ -18,17 +18,49 @@
 			$(".tab-content li").removeClass("active");
 			$(".tab-content li").eq(index).addClass("active");
 		});
-		// 获取滚动条的高度
+		// 监听滚动条事件
 		$(window).scroll(function() {
-		var height= $(document).scrollTop();
-			if(height>185) {
-				$("#logo").hide();
-				$("#language").hide();
-				$("#nav-wrap").css({position:"fixed","border-bottom": "1px solid rgb(221, 221, 221)"});
-			}else{
-				$("#logo").show();
-				$("#language").show();
-				$("#nav-wrap").css({position:"relative",border: 0});
+			while($(window).width()>414){
+				var height= $(document).scrollTop();
+				if(height>185) {
+					$("#logo").hide();
+					$("#language").hide();
+					$("#nav-wrap").css({position:"fixed","border-bottom": "1px solid rgb(221, 221, 221)"});
+				}else{
+					$("#logo").show();
+					$("#language").show();
+					$("#nav-wrap").css({position:"relative",border: 0});
+				}
 			}
 		});
+		//手机端
+		var menuOnPhone = function() {
+			while($(window).width()<=414) {
+				$(".menu").on("click",function() {
+					$("#nav-wrap").fadeIn();
+				});
+				$(".menux").on("click",function() {
+					$("#nav-wrap").fadeOut();
+				})
+			}
+		};
+		menuOnPhone()
+		
+
+		var hoverOnNavForPC = function() {
+			while($(window).width()>414) {
+				$(".nav-list").on("hover",function() {
+					var curSubNav = $(this).find(".sub-nav");
+					// $(".sub-nav").hide();
+					curSubNav.show();
+				});
+			}
+		}
+		hoverOnNavForPC();
+
+		$(window).on("resize",function() {
+			menuOnPhone();
+			hoverOnNavForPC();
+		});
+		
 	});
